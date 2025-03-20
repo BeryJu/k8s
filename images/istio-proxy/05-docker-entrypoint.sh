5f00fe34d18459e0fc8eaf62605b866a6dfebe9e
Jens Langhammer <jens.langhammer@beryju.org>
2025-03-20 02:17:19 +0000

    add support for extra config

--- images/istio-proxy/05-docker-entrypoint.sh
+++ images/istio-proxy/05-docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -eu
-envsubst '${INT_HOST}' < /etc/nginx/conf.d/default.conf.temp > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/conf.d/default.conf.temp > /etc/nginx/conf.d/default.conf
 exec "$@"
